1264942e01452abcd307d33b34afeadfcf3c632b
Richard Jiang Shenhao <richardejiang@hotmail.com>
2014-10-01 02:44:37 +0800

    Added the Tags Methods

--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Task {
+	
+	private static final String MESSAGE_NOT_FOUND = "Not found! Please try again!";
 
 	private String taskName;
+	private ArrayList<String> tags = new ArrayList<String>();
 
+	///////////Tasks Methods/////////////
 	public Task(String taskName) {
 		this.taskName = taskName;
 	}
@@ -12,6 +19,43 @@ public class Task {
 
 	public void setTaskName(String newTaskName) {
 		this.taskName = newTaskName;
+	}
+	
+	///////////Tags Methods/////////////
+	public void addTag(String newTag){
+		tags.add(newTag.toUpperCase());
+		Collections.sort(tags);
+	}
+	
+	public String getTag(int index){
+		if(tags.isEmpty()){
+			return null;
+		}
+		else{
+			return tags.get(index);
+		}
+	}
+	
+	public ArrayList<String> getTags(){
+		return tags;
+	}
+	
+	public boolean hasTag(String tag){
+		if(tags.contains(tag.toUpperCase())){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public void deleteTag(String tag){
+		if(tags.hasTag(tag)){
+			tags.remove(tag.toUpperCase());
+		}
+		else{
+			
+		}
 	}
 	
 }
