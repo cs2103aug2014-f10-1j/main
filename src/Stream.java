1b7d9b65c4ead342afe55a4961876d69605e696a
jkt001 <jk_john_kevin@hotmail.com>
2014-10-03 06:38:52 +0800

    Now it shows a welcome message!

--- src/Stream.java
+++ src/Stream.java
@@ -1,6 +1,7 @@
 public class Stream {
 
 	private static StreamObject st;
+	private static final String MESSAGE_WELCOME = "Welcome to Stream!";
 
 	public void initialize() {
 		st = new StreamObject();
@@ -42,6 +43,16 @@ public class Stream {
 	}
 	
 	/**
+	 * Modify a task's description
+	 * This method is just to differentiate the set new description and modify description part
+	 * @author A0118007R
+	 */
+	
+	public void changeDescription(String task, String newDescription){
+		setDescription(task, newDescription);
+	}
+	
+	/**
 	 * Deletes a specific task
 	 * 
 	 * @author A0118007R
@@ -53,6 +64,10 @@ public class Stream {
 		} catch(Exception e){
 			
 		}
+	}
+	
+	public static void main(String[] args){
+		System.out.println(MESSAGE_WELCOME);
 	}
 
 }
