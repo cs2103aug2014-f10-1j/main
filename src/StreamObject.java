ba3fdc6ce8572a98d0c81f1bddf561ef23e5c3f6
jkt001 <jk_john_kevin@hotmail.com>
2014-10-06 14:55:01 +0800

    some minor changes to the Task constructor (for tags)

--- src/StreamObject.java
+++ src/StreamObject.java
@@ -14,26 +14,23 @@ public class StreamObject {
 	 */
 	private static final String ERROR_TASK_DOES_NOT_EXIST = "Error: The task \"%1$s\" does not exist!";
 
-	private ArrayList<String> tasksList;
 	private HashMap<String, Task> allTasks;
 
 	public StreamObject() {
-		this.tasksList = new ArrayList<String>();
 		this.allTasks = new HashMap<String, Task>();
 	}
 
 	public void addTask(String newTask) throws Exception {
-		if (this.tasksList.contains(newTask)) {
+		if (this.allTasks.containsKey(newTask)) {
 			throw new Exception(String.format(ERROR_TASK_ALREADY_EXISTS,
 					newTask));
 		} else {
-			this.tasksList.add(newTask);
 			this.allTasks.put(newTask, new Task(newTask));
 		}
 	}
 
 	public Boolean hasTask(String task) {
-		return this.tasksList.contains(task);
+		return this.allTasks.containsKey(task);
 	}
 	
 	/**
@@ -61,7 +58,6 @@ public class StreamObject {
 	
 	public void deleteTask(String task) throws Exception{
 		if(hasTask(task)){
-			tasksList.remove(task);
 			allTasks.remove(task);
 		} else {
 			throw new Exception(String.format(ERROR_TASK_DOES_NOT_EXIST, task));
