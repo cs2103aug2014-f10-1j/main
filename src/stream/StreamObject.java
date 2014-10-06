f7af50b2525f123ef518cb633f881e67a2380c3d
dephantom <khongwh@gmail.com>
2014-10-06 23:55:42 +0800

    Implemented skeletal search tests

--- src/stream/StreamObject.java
+++ src/stream/StreamObject.java
@@ -1,12 +1,14 @@
 package stream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 
 public class StreamObject {
 
 	private static String ERROR_TASK_ALREADY_EXISTS = "Error: \"%1$s\" already exists in the tasks list.";
-	
+
 	/**
 	 * String for error when adding a description to a non-existent task
 	 * 
@@ -32,13 +34,12 @@ public class StreamObject {
 	public Boolean hasTask(String task) {
 		return this.allTasks.containsKey(task);
 	}
-	
+
 	/**
 	 * Gets a specific task
 	 * 
 	 * @author A0118007R
 	 */
-	
 	public Task getTask(String task) throws Exception{
 		if (hasTask(task)){
 			return allTasks.get(task);
@@ -47,18 +48,31 @@ public class StreamObject {
 					task));
 		}
 	}
-	
+
 	/**
 	 * Deletes a specific task
 	 * 
 	 * @author A0118007R
 	 */
-	
 	public void deleteTask(String task) throws Exception{
 		if(hasTask(task)){
 			allTasks.remove(task);
 		} else {
 			throw new Exception(String.format(ERROR_TASK_DOES_NOT_EXIST, task));
 		}
+	}
+
+	//@author A0096529N
+	/**
+	 * Search for tasks with specified key phrase, 
+	 * in the task name, description and tags.
+	 * 
+	 * @return tasks - a list of tasks containing 
+	 * the key phrase.
+	 * @author Steven Khong
+	 */
+	public List<Task> findTasks(String keyphrase) {
+		List<Task> tasks = new ArrayList<Task>();
+		return tasks;
 	}
 }
