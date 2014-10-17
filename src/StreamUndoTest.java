0215fc72975748161a2970cb58cb75d54c8b5092
wkurniawan07 <wkurniawan.92@gmail.com>
2014-10-18 06:26:18 +0800

    Refactored and re-organized the Stream class. (1) Main and processAndExecuteInput is separated to doRoutineProcess -> processInput -> executeInput to follow SLAP better. (2) Rewrite some of the Javadocs and also added author tags. (3) Changed visibility: hasTask is package level (used only in testing), main and processInput are both public, everything else private. (4) Found a bug in undo: if we delete and undo, the recovered task is not added in its previous index number (disaster!). Solution: maintain an ordering stack and on top of recovering the lost task, also recover the ordering. (5) Added more assertions.

--- src/StreamUndoTest.java
+++ src/StreamUndoTest.java
@@ -15,24 +15,24 @@ public class StreamUndoTest {
 
 	@Test
 	public void undoAddTest() throws Exception {
-		st.processAndExecute("add do CS2103");
-		st.processAndExecute("add do CS2105");
-		st.processAndExecute("add do CS2106");
-		st.processAndExecute("undo");
+		st.processInput("add do CS2103");
+		st.processInput("add do CS2105");
+		st.processInput("add do CS2106");
+		st.processInput("undo");
 		assertEquals("do CS2103 is included", true, st.hasTask("do CS2103"));
 		assertEquals("do CS2105 is included", true, st.hasTask("do CS2105"));
 		assertEquals("do CS2106 is not included", false,
-				st.hasTask("do CS2106"));		st.processAndExecute("undo");
+				st.hasTask("do CS2106"));		st.processInput("undo");
 
 	}
 
 	@Test
 	public void undoRemoveTest() throws Exception {
-		st.processAndExecute("add do CS2103");
-		st.processAndExecute("add do CS2105");
-		st.processAndExecute("add do CS2106");
-		st.processAndExecute("delete 2");
-		st.processAndExecute("undo");
+		st.processInput("add do CS2103");
+		st.processInput("add do CS2105");
+		st.processInput("add do CS2106");
+		st.processInput("delete 2");
+		st.processInput("undo");
 		assertEquals("do CS2103 is included", true, st.hasTask("do CS2103"));
 		assertEquals("do CS2105 is included", true, st.hasTask("do CS2105"));
 		assertEquals("do CS2106 is included", true, st.hasTask("do CS2106"));
@@ -40,12 +40,12 @@ public class StreamUndoTest {
 
 	@Test
 	public void undoModifyTest() throws Exception {
-		st.processAndExecute("add do CS2103");
-		st.processAndExecute("add do CS2105");
-		st.processAndExecute("add do CS2106");
-		st.processAndExecute("modify 1 do CS2107");
-		st.processAndExecute("modify 1 do CS2104");
-		st.processAndExecute("undo");
+		st.processInput("add do CS2103");
+		st.processInput("add do CS2105");
+		st.processInput("add do CS2106");
+		st.processInput("modify 1 do CS2107");
+		st.processInput("modify 1 do CS2104");
+		st.processInput("undo");
 		assertEquals("do CS2105 is included", true, st.hasTask("do CS2105"));
 		assertEquals("do CS2106 is included", true, st.hasTask("do CS2106"));
 		assertEquals("do CS2107 is included", true, st.hasTask("do CS2107"));
@@ -53,13 +53,13 @@ public class StreamUndoTest {
 
 	@Test
 	public void undoClearTest() throws Exception {
-		st.processAndExecute("add do CS2103");
-		st.processAndExecute("add do CS2104");
-		st.processAndExecute("add do CS2105");
-		st.processAndExecute("add do CS2106");
-		st.processAndExecute("add do CS2107");
-		st.processAndExecute("clear");
-		st.processAndExecute("undo");
+		st.processInput("add do CS2103");
+		st.processInput("add do CS2104");
+		st.processInput("add do CS2105");
+		st.processInput("add do CS2106");
+		st.processInput("add do CS2107");
+		st.processInput("clear");
+		st.processInput("undo");
 		assertEquals("do CS2103 is included", true, st.hasTask("do CS2103"));
 		assertEquals("do CS2104 is included", true, st.hasTask("do CS2104"));
 		assertEquals("do CS2105 is included", true, st.hasTask("do CS2105"));
